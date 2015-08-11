7f0a4a245d3a47f126759265ede9f444ef0e0dde
Simon-Wong <wlsandwho@foxmail.com>
2015-08-11 13:53:26 +0800

    test4

--- TestHello/TestHello.cpp
+++ TestHello/TestHello.cpp
@@ -9,7 +9,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	std::cout<<"Hello World!"<<endl;
 	std::cout<<"Hello World!"<<endl;
 	std::cout<<"Hello World!"<<endl;
-
+	std::cout<<"Hello 1!"<<endl;
+	std::cout<<"Hello 2!"<<endl;
+	std::cout<<"Hello 3!"<<endl;
 	return 0;
 }
 
